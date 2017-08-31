4a27b9e851cc3cb350cb7784f0768dadc304fc72
opol1234 <patcharaporn.th.57@ubu.ac.th>
2017-08-31 11:22:02 +0700

    add index.js

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello I am Patcharaporn Thongrong!');
+    res.end('Hello I am Patcharaporn Thongrong (5711404871)!');
 }).listen(8080);
